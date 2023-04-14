f4d60ddc827c7d7192b85b229e214c59699adb7f
Ali <razaalirafique@gmail.com>
2023-04-14 16:54:57 -0400

    changed the instructions

--- 03_data_visualization.R
+++ 03_data_visualization.R
@@ -64,18 +64,38 @@ ggplot(data = mpg) +
 
 # In the plot below, one group of points (highlighted in red) seems to fall outside of the linear trend. 
 # These cars have a higher mileage than you might expect. How can you explain these cars?
-# Let’s hypothesize that the cars are hybrids. One way to test this hypothesis is to look at the class value for each car. The class variable of the mpg dataset classifies cars into groups such as compact, midsize, and SUV. If the outlying points are hybrids, they should be classified as compact cars or, perhaps, subcompact cars (keep in mind that this data was collected before hybrid trucks and SUVs became popular).
-# You can add a third variable, like class, to a two dimensional scatterplot by mapping it to an aesthetic. An aesthetic is a visual property of the objects in your plot. Aesthetics include things like the size, the shape, or the color of your points. You can display a point (like the one below) in different ways by changing the values of its aesthetic properties. Since we already use the word “value” to describe data, let’s use the word “level” to describe aesthetic properties. Here we change the levels of a point’s size, shape, and color to make the point small, triangular, or blue:
-# You can convey information about your data by mapping the aesthetics in your plot to the variables in your dataset. For example, you can map the colors of your points to the class variable to reveal the class of each car.
+# Let’s hypothesize that the cars are hybrids. One way to test this hypothesis is to look at the class value for each 
+# car. The class variable of the mpg dataset classifies cars into groups such as compact, midsize, and SUV. 
+# If the outlying points are hybrids, they should be classified as compact cars or, perhaps, subcompact 
+# cars (keep in mind that this data was collected before hybrid trucks and SUVs became popular).
+
+# You can add a third variable, like class, to a two dimensional scatterplot by mapping it to an aesthetic. 
+# An aesthetic is a visual property of the objects in your plot. Aesthetics include things like the size, 
+# the shape, or the color of your points. You can display a point (like the one below) in different ways 
+# by changing the values of its aesthetic properties. Since we already use the word “value” to describe data, 
+# let’s use the word “level” to describe aesthetic properties. Here we change the levels of a point’s size, 
+# shape, and color to make the point small, triangular, or blue:
+# You can convey information about your data by mapping the aesthetics in your plot to the variables in 
+# your dataset. For example, you can map the colors of your points to the class variable to reveal the 
+# class of each car.
 
 ggplot(data = mpg) + 
   geom_point(mapping = aes(x = displ, y = hwy, color = class))
 
-# To map an aesthetic to a variable, associate the name of the aesthetic to the name of the variable inside aes(). ggplot2 will automatically assign a unique level of the aesthetic (here a unique color) to each unique value of the variable, a process known as scaling. ggplot2 will also add a legend that explains which levels correspond to which values.
+# To map an aesthetic to a variable, associate the name of the aesthetic to the name of the variable 
+# inside aes(). ggplot2 will automatically assign a unique level of the aesthetic (here a unique color) 
+# to each unique value of the variable, a process known as scaling. ggplot2 will also add a legend that 
+# explains which levels correspond to which values.
 
-# The colors reveal that many of the unusual points are two-seater cars. These cars don’t seem like hybrids, and are, in fact, sports cars! Sports cars have large engines like SUVs and pickup trucks, but small bodies like midsize and compact cars, which improves their gas mileage. In hindsight, these cars were unlikely to be hybrids since they have large engines.
+# The colors reveal that many of the unusual points are two-seater cars. These cars don’t seem like hybrids, 
+# and are, in fact, sports cars! Sports cars have large engines like SUVs and pickup trucks, but small bodies 
+# like midsize and compact cars, which improves their gas mileage. In hindsight, these cars were unlikely to be 
+# hybrids since they have large engines.
 
-# In the above example, we mapped class to the color aesthetic, but we could have mapped class to the size aesthetic in the same way. In this case, the exact size of each point would reveal its class affiliation. We get a warning here, because mapping an unordered variable (class) to an ordered aesthetic (size) is not a good idea.
+# In the above example, we mapped class to the color aesthetic, but we could have mapped class to the size 
+# aesthetic in the same way. In this case, the exact size of each point would reveal its class affiliation. 
+# We get a warning here, because mapping an unordered variable (class) to an ordered aesthetic (size) is 
+# not a good idea.
 
 ggplot(data = mpg) + 
   geom_point(mapping = aes(x = displ, y = hwy, size = class))
