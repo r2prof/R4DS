48b12b9ea2c36bb2acb51933f412cbfcb3e69d09
Ali <razaalirafique@gmail.com>
2023-05-31 14:53:48 -0400

    update

--- 12_tidy_data.R
+++ 12_tidy_data.R
@@ -151,19 +151,47 @@ table2 %>%
 #
 # As you might have guessed from their names, pivot_wider() and pivot_longer() are complements. 
 # pivot_longer() makes wide tables narrower and longer; pivot_wider() makes long tables shorter and wider.
+#
+#--------------------------------
+# 12.3.3 Exercises
+#--------------------------------
+# 1. Why are pivot_longer() and pivot_wider() not perfectly symmetrical?
+#    Carefully consider the following example:
 
+stocks <- tibble(
+  year   = c(2015, 2015, 2016, 2016),
+  half  = c(   1,    2,     1,    2),
+  return = c(1.88, 0.59, 0.92, 0.17)
+)
+stocks %>% 
+  pivot_wider(names_from = year, values_from = return) %>% 
+  pivot_longer(`2015`:`2016`, names_to = "year", values_to = "return")
 
-
-
-
-
-
-
-
-
-
-
-
+# (Hint: look at the variable types and think about column names.)
+# pivot_longer() has a names_ptypes argument, e.g.  names_ptypes = list(year = double()). What does it do?
+#
+# 2. Why does this code fail?
+table4a %>% 
+  pivot_longer(c(1999, 2000), names_to = "year", values_to = "cases")
+#
+# 3. What would happen if you widen this table? Why? How could you add a new column to uniquely 
+#    identify each value?
+people <- tribble(
+  ~name,             ~names,  ~values,
+  #-----------------|--------|------
+  "Phillip Woods",   "age",       45,
+  "Phillip Woods",   "height",   186,
+  "Phillip Woods",   "age",       50,
+  "Jessica Cordero", "age",       37,
+  "Jessica Cordero", "height",   156
+)
+#
+# 4. Tidy the simple tibble below. Do you need to make it wider or longer? What are the variables?
+preg <- tribble(
+  ~pregnant, ~male, ~female,
+  "yes",     NA,    10,
+  "no",      20,    12
+)
 
 
 
