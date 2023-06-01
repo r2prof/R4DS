076f60864379c0831314c87054e631cb0088c564
Ali <razaalirafique@gmail.com>
2023-05-31 22:56:07 -0400

    update

---
+++ 06_workflow_basics.R
@@ -0,0 +1,16 @@
+seq(1, 10)
+x <- "hello world"
+my_variable <- 10
+my_variable
+# 
+# Tweak each of the following R commands so that they run correctly:
+library(tidyverse)
+
+ggplot(data = mpg) + 
+  geom_point(mapping = aes(x = displ, y = hwy))
+
+filter(mpg, cyl == 8)
+
+filter(diamonds, carat > 3)
+
+# Press Alt + Shift + K. What happens? How can you get to the same place using the menus?
